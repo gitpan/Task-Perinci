9c989a19514161a41b703abc9a3ee2644dadb6dd
Steven Haryanto <stevenharyanto@gmail.com>
2012-01-26 09:21:36 +0000

    Import of SHARYANTO/Task-Perinci-0.03 from CPAN.
    gitpan-cpan-distribution: Task-Perinci
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SHARYANTO/Task-Perinci-0.03.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Perinci.pm
@@ -0,0 +1,48 @@
+package Task::Perinci;
+
+our $VERSION = '0.03'; # VERSION
+
+1;
+# ABSTRACT: Install all Perinci:: modules
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::Perinci - Install all Perinci:: modules
+
+=head1 VERSION
+
+version 0.03
+
+=head1 DESCRIPTION
+
+This task will install all Perinci:: modules.
+
+=head1 TASK CONTENTS
+
+=head2 Included modules
+
+=head3 L<Rinci>
+
+=head3 L<Riap>
+
+=head3 L<Perinci>
+
+=head3 L<Perinci::Examples>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
