7d1732af5a855b660eebae6551abccec01a20463
Steven Haryanto <stevenharyanto@gmail.com>
2012-03-23 17:39:13 +0000

    Import of SHARYANTO/Task-Perinci-0.04 from CPAN.
    gitpan-cpan-distribution: Task-Perinci
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         SHARYANTO/Task-Perinci-0.04.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Task/Perinci.pm
+++ lib/Task/Perinci.pm
@@ -1,6 +1,6 @@
 package Task::Perinci;
 
-our $VERSION = '0.03'; # VERSION
+our $VERSION = '0.04'; # VERSION
 
 1;
 # ABSTRACT: Install all Perinci:: modules
@@ -15,7 +15,7 @@ Task::Perinci - Install all Perinci:: modules
 
 =head1 VERSION
 
-version 0.03
+version 0.04
 
 =head1 DESCRIPTION
 
@@ -25,13 +25,47 @@ This task will install all Perinci:: modules.
 
 =head2 Included modules
 
-=head3 L<Rinci>
+=head3 L<Perinci>
+
+=head3 L<Perinci::BashComplete>
+
+=head3 L<Perinci::Examples>
+
+=head3 L<Perinci::Object>
+
+=head3 L<Perinci::Sub::ConvertArgs::Array>
+
+=head3 L<Perinci::Sub::DepChecker>
+
+=head3 L<Perinci::Sub::Gen::AccessTable>
+
+=head3 L<Perinci::Sub::GetArgs::Argv>
+
+=head3 L<Perinci::Sub::GetArgs::Array>
+
+=head3 L<Perinci::Sub::property::drops_privilege>
+
+=head3 L<Perinci::Sub::property::result_postfilter>
+
+=head3 L<Perinci::Sub::property::retry>
+
+=head3 L<Perinci::Sub::property::timeout>
+
+=head3 L<Perinci::Sub::Wrapper>
+
+=head3 L<Perinci::To::HTML>
+
+=head3 L<Perinci::To::POD>
+
+=head3 L<Perinci::To::Text>
+
+=head3 L<Perinci::Use>
+
+=head3 L<Pod::Weaver::Plugin::Perinci>
 
 =head3 L<Riap>
 
-=head3 L<Perinci>
-
-=head3 L<Perinci::Examples>
+=head3 L<Rinci>
 
 =head1 AUTHOR
 
